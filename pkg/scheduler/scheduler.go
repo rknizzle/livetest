fe67585a8952f5ed01b97239f5d7812c42a33493
rknizzle <ryan@autotiv.com>
2020-01-26 16:18:46 -0500

    Added a scheduler package to create new tickers to start...
            ... periodic invocation of jobs

---
+++ pkg/scheduler/scheduler.go
@@ -0,0 +1,40 @@
+// Package scheduler creates tickers to run each job periodically at the correct interval
+
+package scheduler
+
+import (
+	"github.com/rknizzle/testlive/pkg/job"
+	"net/http"
+	"time"
+)
+
+// Create a ticker for a job to trigger it periodically
+func Schedule(j job.Job, interval time.Duration, resChan chan<- *http.Response) *time.Ticker {
+	ticker := time.NewTicker(interval)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				// run the job
+				execute(&j, resChan)
+			}
+		}
+	}()
+	return ticker
+}
+
+// Sends an HTTP request for a job
+func execute(job *job.Job, resChan chan<- *http.Response) {
+	client := &http.Client{}
+
+	// configure the HTTP request
+	r, err := http.NewRequest(job.HTTPMethod, job.URL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// execute the HTTP request
+	resp, _ := client.Do(r)
+	// TODO: pass in and handle errors as well
+	resChan <- resp
+}
