fc67a29db26dc768ee03a03891aea8ef56ca18c3
rknizzle <ryan@autotiv.com>
2020-01-26 16:19:27 -0500

    Added a main function to read in config and start the jobs...
            ... for now it just prints out the response of the HTTP requests
    but it will eventually check the response against an expected response
    to see if the feature is working

---
+++ cmd/livetest/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rknizzle/testlive/pkg/parser"
+	"github.com/rknizzle/testlive/pkg/scheduler"
+	"net/http"
+	"time"
+)
+
+func main() {
+	config := parser.ParseFile("examples/config.json")
+	// buffered response channel
+	// concurrency limit is specified in the config
+	resChan := make(chan *http.Response, config.Concurrency)
+	// loop through each job
+	for _, j := range config.Jobs {
+		// and schedule the job to run at the specified interval
+		scheduler.Schedule(j, time.Duration(j.Frequency)*time.Millisecond, resChan)
+	}
+	for res := range resChan {
+		// just print out the HTTP response for now
+		fmt.Println("got a result")
+		fmt.Println(res)
+	}
+}
