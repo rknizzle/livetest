816114f00d2eeeb7dc872348b0dff62da4141c4c
rknizzle <ryan@autotiv.com>
2020-02-05 22:26:31 -0500

    When database connection fails return error instead of panic

--- pkg/datastore/postgres/postgres.go
+++ pkg/datastore/postgres/postgres.go
@@ -5,6 +5,7 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/rknizzle/livetest/pkg/datastore"
@@ -16,19 +17,19 @@ type Postgres struct {
 }
 
 // Connect to a postgres database and create
-func (p *Postgres) Connect(config *datastore.Connection) {
+func (p *Postgres) Connect(config *datastore.Connection) error {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		config.Host, config.Port, config.User, config.Password, config.DBname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		panic(err)
+		return errors.New("Failed to connect to postgres database")
 	}
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return errors.New("Failed to ping database")
 	}
 	// create the table to store request execution results
 	// to track results over time
@@ -43,10 +44,11 @@ func (p *Postgres) Connect(config *datastore.Connection) {
 
 	_, err = db.Exec(createStatement)
 	if err != nil {
-		panic(err)
+		return errors.New("Failed to execute create statement")
 	}
 	// set the postgres database object
 	p.db = db
+	return nil
 }
 
 // Write a requests execution data to a new row
