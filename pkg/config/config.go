4490c19a2a2b35934997824bb9b86192a9e38a66
rknizzle <ryan@autotiv.com>
2020-02-07 22:41:42 -0500

    Changed Parse function so it can return an error instead of panicing

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,7 +4,7 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
+	"errors"
 	"github.com/rknizzle/livetest/pkg/datastore"
 	"github.com/rknizzle/livetest/pkg/job"
 	"github.com/rknizzle/livetest/pkg/notification"
@@ -33,38 +33,48 @@ type Envelope struct {
 	Msg  *json.RawMessage `json:"msg"`
 }
 
-func initializeNotification(e Envelope) notification.Notification {
+// take in notification data from config and return either a Notification object,
+// an error if something goes wrong, or nil if there was no notification specified in config
+func initializeNotification(e Envelope) (notification.Notification, error) {
+	// if envelope is empty return nil notification
+	if e == (Envelope{}) {
+		return nil, nil
+	}
 	switch e.Type {
 	case "http":
 		var h notification.HTTPRequest
 		if err := json.Unmarshal(*e.Msg, &h); err != nil {
-			panic(err)
+			return nil, errors.New("Couldn't parse HTTP notification data from config")
 		}
-		return h
+		return h, nil
 	default:
-		fmt.Println("No valid notification type in config. Running without notification.")
-		return nil
+		// return error if the notification type is not supported
+		return nil, errors.New(e.Type + " is not a supported notification type")
 	}
 }
 
 // Parses the specified config file and loads the data into a config object
-func Parse(filepath string) *Config {
+func Parse(filepath string) (*Config, error) {
 	var pre Pre
 	// read in the config file
 	configFile, err := os.Open(filepath)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	jsonParser := json.NewDecoder(configFile)
 	err = jsonParser.Decode(&pre)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	n := initializeNotification(pre.Notification)
+	// initialize notification from config value
+	n, err := initializeNotification(pre.Notification)
+	if err != nil {
+		return nil, err
+	}
 
 	// create the config object from JSON data
 	c := &Config{pre.Jobs, pre.Concurrency, pre.Datastore, n}
 
-	return c
+	return c, nil
 }
